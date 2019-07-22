b07d08eff5a1c6d8c637d51f77bdd75bc5768afa
fransdihan <digb.adi@gmail.com>
2019-07-22 17:30:54 +0700

    Add project files.

---
+++ ConvertDocxToPdf/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports Telerik.Windows.Documents.Common.FormatProviders
+Imports Telerik.Windows.Documents.Flow.FormatProviders.Docx
+Imports Telerik.Windows.Documents.Flow.FormatProviders.Pdf
+Imports Telerik.Windows.Documents.Flow.Model
+
+Module Module1
+
+    Sub Main()
+        Dim path As String = "test.docx"
+        File.SetAttributes(path, FileAttributes.Normal)
+
+        Dim document As RadFlowDocument
+
+        Using stream As Stream = New FileStream(path, FileMode.Open)
+            document = New DocxFormatProvider().Import(stream)
+        End Using
+
+        Dim formatProvider As IFormatProvider(Of RadFlowDocument) = New PdfFormatProvider()
+
+        Using stream As Stream = New FileStream("test.pdf", FileMode.CreateNew)
+            formatProvider.Export(document, stream)
+        End Using
+
+    End Sub
+
+End Module
